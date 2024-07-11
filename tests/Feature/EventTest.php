b8876d11fdbf995d5ab11c7108539b3289ef79e3
iqbal <iqbalsborneo@gmail.com>
2024-07-11 11:24:01 +0700

    Commit 11-7-24 11.23

--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -2,17 +2,26 @@
 
 namespace Tests\Feature;
 
+use App\Models\Event;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class EventTest extends TestCase
 {
-    use RefreshDatabase;
+    // use RefreshDatabase;
+    use WithFaker;
+
+    protected $baseUrl;
+    public function setup() : void
+    {
+        parent::setup();
+        $this->baseUrl = 'events';
+    }
 
     public function test_can_show_event_page()
     {
-        $this->get('')->assertOk();
+        $this->get('/')->assertOk();
     }
 
     public function test_can_show_create_event_page()
@@ -22,6 +31,48 @@ class EventTest extends TestCase
         ->assertSee('title')
         ->assertSee('category')
         ->assertSee('end_date');
+    }
+
+     public function test_success_store_event()
+    {
+        $this->postJson($this->baseUrl, [
+            'title' => $this->faker->sentence(3),
+            'start_date' => date('Y-m-d'),
+            'end_date' => date('Y-m-d'),
+            'category' => $this->faker->randomElement(['success', 'danger', 'warning', 'info']),
+        ])->assertOk();
+    }
+
+    public function test_success_update_event()
+    {
+        $this->postJson($this->baseUrl, [
+            'title' => $this->faker->sentence(3),
+            'start_date' => date('Y-m-d'),
+            'end_date' => date('Y-m-d'),
+            'category' => $this->faker->randomElement(['success', 'danger', 'warning', 'info']),
+        ])->assertOk();
+
+        $event = Event::orderByDesc('id')->first();
+        $this->putJson($this->baseUrl. '/'. $event->id, $this->data())
+        ->assertStatus(200);
+    }
+
+    private function data()
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'start_date' => date('Y-m-d'),
+            'end_date' => date('Y-m-d'),
+            'category' => $this->faker->randomElement(['success', 'danger', 'warning', 'info']),
+        ];
+    }
+
+    public function test_validation_store_event()
+    {
+        $this->postJson($this->baseUrl)->assertStatus(422)
+        ->assertSeeInOrder([
+            'start_date', 'end_date', 'title', 'category'
+        ]);
     }
 
 }
